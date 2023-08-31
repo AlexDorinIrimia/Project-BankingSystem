dcd71f4c0d95da96a64ff513ccfbfd576b9235f1
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-08-31 13:46:22 +0300

    Tranzactii-implementare clasa

--- Tranzactii.cpp
+++ Tranzactii.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Tranzactii.h"
+# include "Cont.h"
 
 Tranzactii::Tranzactii(std::string desc, int val) {
     this->descriere = desc;
@@ -11,8 +12,20 @@ Tranzactii::Tranzactii(std::string desc, int val) {
 
 Tranzactii creareTranzactie(std::string desc, int val){
     Tranzactii tr = Tranzactii(desc,val);
+    tr.ID = "Tr" + std::to_string(rand());
     return tr;
 }
 
 [[maybe_unused]] std::string Tranzactii::getDesc() {return this->descriere;}
 [[maybe_unused]] int Tranzactii::getValoare() {return this->valoare;}
+[[maybe_unused]] std::string Tranzactii::getID() {return this->ID;}
+[[maybe_unused]]void Tranzactii::depunere(int val, Cont cnt){
+    cnt.setSold(cnt.getSold() + val);
+}
+[[maybe_unused]]void Tranzactii::retragere(int val, Cont cnt){
+    cnt.setSold(cnt.getSold() - val);
+}
+[[maybe_unused]]void Tranzactii::transfer(Cont cont1, Cont cont2, int suma){
+    retragere(suma,cont1);
+    depunere(suma,cont2);
+}
