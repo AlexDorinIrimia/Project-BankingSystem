6090801547ee38ff65e885cb651b3cce7e7fb9a9
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-01 17:10:28 +0300

    Alte functii necesare

---
+++ Utilities.h
@@ -0,0 +1,14 @@
+//
+// Created by alex on 01.09.2023.
+//
+
+#ifndef PROIECT_SISTEMBANCAR_UTILITIES_H
+#define PROIECT_SISTEMBANCAR_UTILITIES_H
+
+# include "Cont.h"
+
+void vizualizareDetalii(Cont cnt);
+bool LogIn();
+std::string inputPass();
+
+#endif //PROIECT_SISTEMBANCAR_UTILITIES_H
