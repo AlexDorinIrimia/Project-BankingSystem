6fffb116d360072f98e2b2f9793d4cbe35e8d5fd
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-08 17:33:24 +0300

    Client - fisierheader clasa

--- Client.h
+++ Client.h
@@ -13,22 +13,20 @@ private:
     std :: string nume;
     std :: string adresa;
     std :: string numarDeTelefon;
-    std :: string parola;
 
 public:
-    Client(std :: string CNP,  std :: string nume,  std :: string adresa,  std :: string numarDeTelefon,  std :: string parola);
-    Client creareClient(std :: string CNP, std :: string nume, std :: string adresa, std :: string numarDeTelefon, std :: string parola);
+    Client(std :: string CNP,  std :: string nume,  std :: string adresa,  std :: string numarDeTelefon);
+    Client creareClient(std :: string CNP, std :: string nume, std :: string adresa, std :: string numarDeTelefon);
 
     [[maybe_unused]] std :: string getCNP();
     [[maybe_unused]] std :: string getNume();
     [[maybe_unused]] std :: string getAdresa();
     [[maybe_unused]] std :: string getNumarDeTelefon();
-    [[maybe_unused]] std :: string getParola();
+
     void setCnp(std::string cnp);
     void setNume(std::string nume);
     void setAdresa(std::string adresa);
     void setNumarDeTelefon(std::string numarDeTelefon);
-    void setParola(std::string parola);
 
     Client();
 };
