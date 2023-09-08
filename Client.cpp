dad6c915580e2aa811a24dcad80dcc230deb9ead
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-08 17:33:40 +0300

    Client - implementare clasa

--- Client.cpp
+++ Client.cpp
@@ -6,17 +6,16 @@
 
 #include <utility>
 
-Client::Client(std::string cnp, std::string name, std::string adress, std::string numardetelefon, std::string pass) {
+Client::Client(std::string cnp, std::string name, std::string adress, std::string numardetelefon) {
     this->CNP = std::move(cnp);
     this->nume = std::move(name);
     this->adresa = std::move(adress);
     this->numarDeTelefon = std::move(numardetelefon);
-    this->parola = std::move(pass);
 }
 
-[[maybe_unused]] Client Client::creareClient(std :: string cnp,  std :: string name,  std :: string adress,  std :: string numardetelefon,  std :: string pass)
+[[maybe_unused]] Client Client::creareClient(std :: string cnp,  std :: string name,  std :: string adress,  std :: string numardetelefon)
 {
-    Client client = Client(cnp,name,adress,numardetelefon,pass);
+    Client client = Client(cnp,name,adress,numardetelefon);
     return client;
 }
 
@@ -24,11 +23,9 @@ Client::Client(std::string cnp, std::string name, std::string adress, std::strin
 [[maybe_unused]]std::string Client::getNume() {return this->nume;}
 [[maybe_unused]]std::string Client::getAdresa() {return this->adresa;}
 [[maybe_unused]]std::string Client::getNumarDeTelefon() {return this->numarDeTelefon;}
-[[maybe_unused]]std::string Client::getParola() {return this->parola;}
 void Client::setCnp(std::string cnp) {this->CNP = cnp;}
 void Client::setNume(std::string nume) {this->nume = nume;}
 void Client::setAdresa(std::string adresa) {this->adresa = adresa;}
 void Client::setNumarDeTelefon(std::string numarDeTelefon) {this->numarDeTelefon = numarDeTelefon;}
-void Client::setParola(std::string parola) {this->parola = parola;}
 
 Client::Client() {}
