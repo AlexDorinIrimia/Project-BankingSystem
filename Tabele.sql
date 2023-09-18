a2d6ed8d37e42f8a027075fcf30476d03bd0e885
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-18 18:20:16 +0300

    Fisier .sql tabele baza de date

--- Tabele.sql
+++ Tabele.sql
@@ -1,25 +1,22 @@
-CREATE TABLE Client(
-    CNP VARCHAR(10) NOT NULL,
-    Nume VARCHAR(30) NOT NULL,
-    Adresa VARCHAR(30),
-    'Numar de telefon' VARCHAR(10),
-    Parola VARCHAR(20) NOT NULL,
-    constraint client_pk PRIMARY KEY(CNP)
+CREATE TABLE IF NOT EXISTS Client(
+    CNP VARCHAR(10) PRIMARY KEY NOT NULL,
+    Nume VARCHAR(20) NOT NULL,
+    Adresa VARCHAR(30) NOT NULL,
+    'Numar de telefon' VARCHAR(12) NOT NULL,
+    Parola VARCHAR(16) NOT NULL
 );
 
-CREATE TABLE Conturi(
-    'Numar Cont' VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS CONTURI(
+    'Numar cont' VARCHAR(20) PRIMARY KEY NOT NULL,
     CNP VARCHAR(10) NOT NULL,
-    Sold NUMERIC(8,3),
-    constraint conturi_pk PRIMARY KEY('Numar Cont'),
-    constraint client_pk FOREIGN KEY (CNP) REFERENCES Client(CNP)
+    Sold REAL,
+    FOREIGN KEY(CNP) REFERENCES Client(CNP)
 );
 
-CREATE TABLE Tranzactii(
-    ID VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS Tranzactii(
+    ID VARCHAR(20) PRIMARY KEY NOT NULL,
     'Numar Cont' VARCHAR(20) NOT NULL,
-    Valoare NUMERIC(8,3),
-    Descriere Varchar(30),
-    constraint tranzactiii_pk PRIMARY KEY(ID),
-    constraint conturi_pk FOREIGN KEY ('Numar Cont') REFERENCES Conturi('Numar Cont')
+    Sold REAL,
+    Descriere VARCHAR(60),
+    FOREIGN KEY('Numar Cont') REFERENCES CONTURI('Numar Cont')
 );
