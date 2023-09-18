a73801268790172ba6a5f2d6bbb2d6cb99c48276
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-18 18:25:06 +0300

    Header alte functii

--- Utilities.h
+++ Utilities.h
@@ -5,11 +5,14 @@
 #ifndef PROIECT_SISTEMBANCAR_UTILITIES_H
 #define PROIECT_SISTEMBANCAR_UTILITIES_H
 
+# include <string>
 # include "Cont.h"
 # include "Client.h"
 
 void vizualizareDetalii(Cont cnt);
-bool LogIn(std::string numeClient, std::string pass);
-std::string inputPass();
-
+bool LogIn(Client *client);
+bool cautareCont(Client cl,Cont *cnt);
+Cont cautareCont1(std::string nume,Cont *cnt);
+void ServiciiBancare(Cont *c);
+void schibareInformatiiClient(Client *client);
 #endif //PROIECT_SISTEMBANCAR_UTILITIES_H
