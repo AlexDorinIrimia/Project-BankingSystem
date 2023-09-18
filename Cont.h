6a60786b119b78d240e6abc695725446c00c87ed
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-09-18 17:27:01 +0300

    Cont - fisier header clasa

--- Cont.h
+++ Cont.h
@@ -1,7 +1,6 @@
 //
 // Created by alex on 30.08.2023.
 //
-
 #ifndef PROIECT_SISTEMBANCAR_CONT_H
 #define PROIECT_SISTEMBANCAR_CONT_H
 
@@ -12,16 +11,16 @@ private:
     std::string numarCont;
     std :: string clientCNP;
     int sold;
+
 public:
     Cont(std::string cl);
-    Cont creareCont(std::string  cl);
+
     [[maybe_unused]] std :: string getNumarCont();
     [[maybe_unused]] std::string getClient();
     [[maybe_unused]] int getSold();
     void setNumarCont(std::string numarCont);
     void setClientCnp(std::string clientCnp);
     void setSold(int sold);
-
     Cont();
 };
 
